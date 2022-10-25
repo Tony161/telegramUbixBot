d26a1850ec72b7bdce8076523dd3db5d22d97fb9
Roms <roms10885@yandex.ru>
2022-10-25 17:26:10 +0300

    feat: main

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import TelegramAPi from 'node-telegram-bot-api'
-import config from 'config'
+import TelegramBot  from 'node-telegram-bot-api';
+import * as dotenv from 'dotenv'
 
-const token = config.get('token')
+import request from 'request';
+import express from 'express';
 
-const bot = new TelegramAPi(token, {polling: true})
+const app = express();
+console.log(process.env.BOT_API_KEY)
+const bot = new TelegramBot(process.env.BOT_API_KEY, { polling: true });
+dotenv.config();
+
 
 let fl = 0
 
@@ -26,3 +31,39 @@ bot.on('message', async msg => {
     await bot.sendMessage(id, html, {parse_mode: 'HTML'})
   }
 })
+
+bot.onText(/\/movie (.+)/, (msg, match) => {
+  let movie = match[1];
+  let chatId = msg.chat.id;
+  request(`http://www.omdbapi.com/?apiKey=${process.env.OMD_API_KEY}=${movie}`, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+
+      // bot.sendMessage(chatId, '_Looking for _' + movie + '...', { parse_mode: 'Markdown' })
+      //   .then((msg) => {
+      //     let res = JSON.parse(body);
+      //     bot.sendPhoto(chatId, res.Poster, { caption: 'Result: \nTitle: ' + res.Title + '\nYear: ' + res.Year + '\nRated: ' + res.Rated + '\nReleased: ' + res.Released + '\nRuntime: ' + res.Runtime + '\nGenre: ' + res.Genre + '\nDirector: ' + res.Director + '\nPlot: ' + res.Plot })
+      //       .catch((err) => {
+      //         if (err) {
+      //           bot.sendMessage(chatId, 'Error in finding,Check the movie title');
+      //         }
+      //       })
+      //   })
+
+
+    }
+  })
+})
+
+// bot.onText(/\/about (.+)/, (msg, match) => {
+//   if (match[1]) {
+//     let chatId = msg.chat.id;
+//     bot.sendMessage(chatId,
+//       `finder bot\nbot where you can search for a movie and get details about it.`, { parse_mode: 'Markdown' });
+//   }
+// })
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`Server is running at port ${PORT}`);
+});
