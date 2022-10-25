6fa7a0c14eb16d8d1c5bfb0dc7b1e4c09d9438b0
Roms <roms10885@yandex.ru>
2022-10-25 11:44:51 +0300

    feat: add html

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 import TelegramAPi from 'node-telegram-bot-api'
 import config from 'config'
 
-const token = config.get('token') || '5567160739:AAHD-EyIN8yqM2a9YMm6NA92kkMK7Hrtd3Q'
+const token = config.get('token')
 
 const bot = new TelegramAPi(token, {polling: true})
 
 let fl = 0
-const start = () => {
-  bot.on('message', async msg => {
-    const {text, chat: {id}} = msg
-    try{
-      if (text === '/start') {
-        return bot.sendMessage(id, `Hello ${msg.from.first_name}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`)
-      }
 
-      if(msg.from ) {
-        fl = 1
-      }
-      console.log('@@@@@ here',msg.from, fl)
-      if(fl) {
-        return bot.sendMessage(id,`Thank your for the interest ${msg.from.first_name}, we'll contact you soon! <a href="t.me/maxim_tgn" >ddd</a>`)
-        fl = 0
-      }
-    } catch(e){
-        return bot.sendMessage(id,`Error send message`)
-      }
+bot.on('message', async msg => {
+  const {text, chat: {id}} = msg
 
+  if (text === '/start') {
+    await bot.sendMessage(id, `Hello ${msg.from.first_name}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`)
+  }
 
-  })
-}
+  if (msg.text !== '/start') {
+    fl = 1
+  }
 
-start()
+  if (fl) {
+    fl = 0
+    const html = `<strong>Thank your for the interest ${msg.from.first_name}, we'll contact you soon! Feel free to ask any question in our telegram channel:</strong>
+        <a href='https://t.me/+q8tzqV5TCmw4NTA6'>testChannel</a>`
 
+    await bot.sendMessage(id, html, {parse_mode: 'HTML'})
+  }
+})
