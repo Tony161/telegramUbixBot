14c4f05bcb9e9f71c5e5d717d7c84182db22d901
Roms <roms10885@yandex.ru>
2022-10-27 13:02:32 +0300

    feat: main

---
+++ bot.js
@@ -0,0 +1,49 @@
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const token = process.env.TELEGRAM_TOKEN;
+
+let fl = 0
+let bot;
+
+
+// if production env, we use webhooks
+// https://core.telegram.org/bots/api#setwebhook
+// https://github.com/yagop/node-telegram-bot-api/blob/release/doc/api.md#TelegramBot+setWebHook
+if (process.env.NODE_ENV === 'production') {
+  bot = new TelegramBot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  console.log('**** BOT initiated ***** ');
+} else {
+  // otherwise, we use polling
+  // differences between webhooks and polling:
+  // https://core.telegram.org/bots/webhooks
+  // https://stackoverflow.com/questions/40033150/telegram-bot-getupdates-vs-setwebhook
+  bot = new TelegramBot(token, { polling: true });
+}
+
+console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
+
+bot.on('message', async (msg) => {
+  commands = ['/start', '/help'];
+  const {text, chat: {id}} = msg
+
+ if (text === '/start') {
+    await bot.sendMessage(id, `Hello ${msg.from.first_name}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`)
+  }
+
+  if (msg.text !== '/start') {
+    fl = 1
+   }
+
+  if (fl) {
+    fl = 0
+  const html = `<strong>Thank your for the interest ${msg.from.first_name}, we'll contact you soon! Feel free to ask any question in our telegram channel:</strong>
+        <a href='https://t.me/+q8tzqV5TCmw4NTA6'>testChannel</a>`
+
+   await bot.sendMessage(id, html, {parse_mode: 'HTML'})
+  }
+});
+
+module.exports = bot;
--- index.js
+++ index.js
@@ -1,66 +1,21 @@
-import TelegramBot  from 'node-telegram-bot-api';
-import * as dotenv from 'dotenv'
-dotenv.config();
-import request from 'request';
-import express from 'express';
+const dotenv = require('dotenv')
+ dotenv.config();
+const bot= require('./bot.js')
+const express= require('express');
 
 const app = express();
-console.log(process.env.BOT_API_KEY)
-const bot = new TelegramBot(process.env.BOT_API_KEY, { polling: true });
-
-let fl = 0
-
-bot.on('message', async msg => {
-  const {text, chat: {id}} = msg
-
-  if (text === '/start') {
-    await bot.sendMessage(id, `Hello ${msg.from.first_name}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`)
-  }
-
-  if (msg.text !== '/start') {
-    fl = 1
-  }
-
-  if (fl) {
-    fl = 0
-    const html = `<strong>Thank your for the interest ${msg.from.first_name}, we'll contact you soon! Feel free to ask any question in our telegram channel:</strong>
-        <a href='https://t.me/+q8tzqV5TCmw4NTA6'>testChannel</a>`
-
-    await bot.sendMessage(id, html, {parse_mode: 'HTML'})
-  }
-})
-
-// bot.onText(/\/movie (.+)/, (msg, match) => {
-//   let movie = match[1];
-//   let chatId = msg.chat.id;
-//   request(`http://www.omdbapi.com/?apiKey=${process.env.OMD_API_KEY}=${movie}`, (error, response, body) => {
-//     if (!error && response.statusCode == 200) {
-//
-//       // bot.sendMessage(chatId, '_Looking for _' + movie + '...', { parse_mode: 'Markdown' })
-//       //   .then((msg) => {
-//       //     let res = JSON.parse(body);
-//       //     bot.sendPhoto(chatId, res.Poster, { caption: 'Result: \nTitle: ' + res.Title + '\nYear: ' + res.Year + '\nRated: ' + res.Rated + '\nReleased: ' + res.Released + '\nRuntime: ' + res.Runtime + '\nGenre: ' + res.Genre + '\nDirector: ' + res.Director + '\nPlot: ' + res.Plot })
-//       //       .catch((err) => {
-//       //         if (err) {
-//       //           bot.sendMessage(chatId, 'Error in finding,Check the movie title');
-//       //         }
-//       //       })
-//       //   })
-//
-//
-//     }
-//   })
-// })
-
-// bot.onText(/\/about (.+)/, (msg, match) => {
-//   if (match[1]) {
-//     let chatId = msg.chat.id;
-//     bot.sendMessage(chatId,
-//       `finder bot\nbot where you can search for a movie and get details about it.`, { parse_mode: 'Markdown' });
-//   }
-// })
-
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from the UBIX Bot API.' });
+});
+// TELEGRAM WEBHOOK - https://core.telegram.org/bots/api#setwebhook
+app.post(`/${process.env.TELEGRAM_TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.status(200).json({ message: 'ok' });
+});
 
 app.listen(PORT, function () {
   console.log(`Server is running at port ${PORT}`);
