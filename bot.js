c651a3b03c67bb7620d94345c326a15224b8bf6b
Roms <roms10885@yandex.ru>
2022-11-07 17:45:01 +0300

    feat: add location

--- bot.js
+++ bot.js
@@ -48,6 +48,20 @@ bot.on('message', async (msg) => {
   }
 });
 
+bot.onText(/location/, (msg) => {
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{text: 'Location', request_location: true}],
+        [{text: 'Contact', request_contact: true}],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    }),
+  };
+  bot.sendMessage(msg.chat.id, 'Contact and Location request', opts);
+});
+
 bot.on('location', (msg) => {
   console.log(msg)
   console.log(msg.message.location.latitude);
