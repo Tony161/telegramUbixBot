8ebc40b10f4e0b4a40842a0a934c21dbc0c4c68f
Roms <roms10885@yandex.ru>
2022-11-07 12:37:24 +0300

    feat: clean master

--- bot.js
+++ bot.js
@@ -7,7 +7,6 @@ const token = process.env.TELEGRAM_TOKEN;
 let fl = 0
 let bot;
 
-
 // if production env, we use webhooks
 // https://core.telegram.org/bots/api#setwebhook
 // https://github.com/yagop/node-telegram-bot-api/blob/release/doc/api.md#TelegramBot+setWebHook
@@ -26,82 +25,22 @@ if (process.env.NODE_ENV === 'production') {
 console.log(`Bot started in the ${process.env.NODE_ENV} mode`);
 
 bot.on('message', async (msg) => {
-  commands = ['/start', '/info'];
-  const {text, chat: {id}} = msg
+  commands = ['/start'];
+  const {text, chat: {id}, from:{first_name, username }} = msg
 
  if (text === '/start') {
-    await bot.sendMessage(id, `Hello ${msg.from.first_name}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`)
+    await bot.sendMessage(id, `Hello ${username}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`)
+  } else {
+   fl = 1
+ }
+
+  if (fl) {
+    fl = 0
+  const html = `<strong>Thank your for the interest ${username}, we'll contact you soon! Feel free to ask any question in our telegram channel:</strong>
+        <a href='https://t.me/+q8tzqV5TCmw4NTA6'>testChannel</a>`
+
+    await bot.sendMessage(id, html, {parse_mode: 'HTML'})
   }
-
-  if (msg.text == '/info') {
-
-    var chat_id = 'https://t.me/+q8tzqV5TCmw4NTA6' //msg.chat.id;
-
-    var date = (new Date(2022, 10, 28)).getTime(); // Timestamp date
-
-    // Options
-    var ops = {
-      expire_date: Date.now + 3600000, //date,
-      member_limit: 1
-    }
-
-    bot.createChatInviteLink(chat_id).then((data) => {
-      console.log(data);
-
-      // bot.editChatInviteLink(chat_id, data.invite_link, { member_limit: 3 }).then(data2 => {
-      //   console.log(data2);
-      //
-      //   bot.revokeChatInviteLink(chat_id, data2.invite_link).then(data3 => {
-      //     console.log(data3);
-      //   });
-      // });
-    });
-
-  }
-
-  // if (msg.text !== '/start') {
-  //   fl = 1
-  //  }
-  //
-  // if (fl) {
-  //   fl = 0
-  // const html = `<strong>Thank your for the interest ${msg.from.first_name}, we'll contact you soon! Feel free to ask any question in our telegram channel:</strong>
-  //       <a href='https://t.me/+q8tzqV5TCmw4NTA6'>testChannel</a>`
-  //
-  //   await bot.sendMessage(id, html, {parse_mode: 'HTML'})
-  // }
-
-
-
-});
-
-bot.onText(/link/, (msg) => {
-
-  var chat_id = 'https://t.me/+q8tzqV5TCmw4NTA6' //msg.chat.id;
-
-
-  var date = (new Date(2022, 10, 28)).getTime(); // Timestamp date
-
-  // Options
-  var ops = {
-    expire_date: Date.now + 3600000, //date,
-    member_limit: 1
-  }
-
-  bot.createChatInviteLink(chat_id, ops).then((data) => {
-    console.log(data);
-
-    // bot.editChatInviteLink(chat_id, data.invite_link, { member_limit: 3 }).then(data2 => {
-    //   console.log(data2);
-    //
-    //   bot.revokeChatInviteLink(chat_id, data2.invite_link).then(data3 => {
-    //     console.log(data3);
-    //   });
-    // });
-
-
-  });
-
 });
 
 module.exports = bot;
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const dotenv = require('dotenv')
  dotenv.config();
 const bot= require('./bot.js')
 const express= require('express');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from the UBIX Bot API.' });
