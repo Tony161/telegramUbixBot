df593211a401a4f672cca83a9a6d9274bba3fa4f
Roms <roms10885@yandex.ru>
2022-11-21 15:32:41 +0300

    feat: add pooling time

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/', (req, res) => {
 });
 
 app.post(`/${process.env.TELEGRAM_TOKEN}`, (req, res) => {
+  console.log('@@@@@ req.body',req.body)
   bot.processUpdate(req.body);
   res.status(200).json({ message: 'ok' });
 });
