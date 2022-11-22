b2a279645622d521d546a900b503cb0f880e1331
Roms <roms10885@yandex.ru>
2022-11-22 15:53:01 +0300

    feat: prettier

--- bot.js
+++ bot.js
@@ -1,45 +1,57 @@
 const TelegramBot = require('node-telegram-bot-api');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config();
-const Tick = require('tick-tock')
-  , tock = new Tick();
+const Tick = require('tick-tock'),
+    tock = new Tick();
 
 const token = process.env.TELEGRAM_TOKEN;
-const chatId = process.env.CHAT_ID
-const interval =`${process.env.POLLING_TIME} minutes`
+const chatId = process.env.CHAT_ID;
+const interval = `${process.env.POLLING_TIME} minutes`;
 let bot;
 
 if (process.env.NODE_ENV === 'production') {
-  bot = new TelegramBot(token);
-  bot.setWebHook(process.env.HOST_URL + bot.token);
+    bot = new TelegramBot(token);
+    bot.setWebHook(process.env.HOST_URL + bot.token);
 } else {
-  bot = new TelegramBot(token, { polling: true });
+    bot = new TelegramBot(token, {polling: true});
 }
 
-bot.on('message', async (msg) => {
-  try {
-    commands = ['/start'];
-    const {text, message_id, chat: {id}, from: {username}} = msg
+bot.on('message', async msg => {
+    try {
+        commands = ['/start'];
+        const {
+            text,
+            message_id,
+            chat: {id},
+            from: {username}
+        } = msg;
 
-    if (text === '/start') {
-      await bot.sendMessage(id, `Hello ${username}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`)
-    } else {
-      const html = `<strong>Thank your for the interest ${username}, we'll contact you soon! Feel free to ask any question in our telegram channel: </strong>`
-      await bot.sendMessage(id, html, {parse_mode: 'HTML'})
-      await bot.forwardMessage(chatId, id, message_id)
+        if (text === '/start') {
+            await bot.sendMessage(
+                id,
+                `Hello ${username}, if you're interested in contributing for our project please send UIP task number, your GitHub account name and tell us a few words about your software development experience`
+            );
+        } else {
+            const html = `<strong>Thank your for the interest ${username}, we'll contact you soon! Feel free to ask any question in our telegram channel: </strong>`;
+            await bot.sendMessage(id, html, {parse_mode: 'HTML'});
+            await bot.forwardMessage(chatId, id, message_id);
+        }
+    } catch {
+        tock.clear();
     }
-  } catch {
-    tock.clear();
-  }
 });
 
-tock.setInterval('poolBot', () =>bot.sendChatAction(chatId, "typing" ).then(()=>  bot.sendMessage(chatId, "WorK it, Maxim", )), interval)
+tock.setInterval(
+    'poolBot',
+    () => bot.sendChatAction(chatId, 'typing').then(() => bot.sendMessage(chatId, 'WorK it, Maxim')),
+    interval
+);
 
 bot.on('polling_error', () => {
-  tock.clear();
+    tock.clear();
 });
 bot.on('webhook_error', () => {
-  tock.clear();
+    tock.clear();
 });
 
 module.exports = bot;
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const dotenv = require('dotenv')
- dotenv.config();
-const bot= require('./bot.js')
-const express= require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const bot = require('./bot.js');
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the UBIX Bot API.' });
+    res.status(200).json({message: 'Hello from the UBIX Bot API.'});
 });
 
 app.post(`/${process.env.TELEGRAM_TOKEN}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.status(200).json({ message: 'ok' });
+    bot.processUpdate(req.body);
+    res.status(200).json({message: 'ok'});
 });
 
 app.listen(PORT, function () {
-  console.log(`Server is running at port ${PORT}`);
+    console.log(`Server is running at port ${PORT}`);
 });
